38c32824f75732d1ee5956fe770dd547c67240af
shey <shey@shey.ca>
2025-06-15 13:56:14 -0400

    only parse gemfile.lock files, skip gemspec and gemfiles

--- app/models/projects/gemfile_scraper.rb
+++ app/models/projects/gemfile_scraper.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-# This has gone through so many iterations.
-#
-# It's best just to look at the Gemfile.lock
-# See https://github.com/SquareSquash/web for an example of a not-so-friendly thing to parse
-# and the ruby that gets evaled in a gemfile can screw this up too.
-#
 module Projects
   class GemfileScraper < GithubScraper
     attr_reader :gems
@@ -15,79 +9,18 @@ module Projects
       @gems = []
     end
 
-    # some weird ones:
-    # https://github.com/instructure/canvas-lms/blob/master/Gemfile.d/app.rb
-    # https://github.com/decidim/decidim
-    # https://github.com/owen2345/camaleon-cms
-    # https://github.com/solidusio/solidus/blob/master/core/solidus_core.gemspec
     def run
-      files = if @project.gems_path.present?
-                @project.gems_path.split(",").map { |path| path.strip.split("/") }
-              else
-                [['Gemfile.lock']]
-              end
+      ref = @project.branch.presence || @project.last_commit
+      url = "https://api.github.com/repos/#{@project.github}/contents/Gemfile.lock?ref=#{ref}"
+      response = get(url)
 
-      files.each do |parts|
-        content_item = nil
-        next_url = "https://api.github.com/repos/#{@project.github}/git/trees/#{@project.last_commit}"
-        parts.each_with_index do |part, index|
-          response = get(next_url)
-          json = JSON.parse(response.to_s)
-          type = index == (parts.size - 1) ? 'blob' : 'tree'
-          next_file = json['tree'].detect { |tree| tree['path'] == part && tree['type'] == type }
-          if type == 'blob'
-            content_item = next_file
-            break
-          else
-            next_url = next_file['url']
-          end
-        end
-
-        return unless content_item
-        content_response = get(content_item['url'])
-        content = Base64.decode64(content_response.parsed_response['content']).force_encoding('UTF-8')
-
-        parse_file(content, content_item['path'])
+      unless response.success?
+        Rails.logger.warn("Gemfile.lock not found for #{@project.github} at ref #{ref}")
+        return
       end
 
-      @gems.flatten!
-    end
-
-    private
-
-    def parse_file(content, filename)
-      if filename.end_with?('gemspec')
-        parse_gemspec(content)
-      elsif filename.end_with?('.lock')
-        parse_lockfile(content)
-      elsif filename.end_with?('.rb') || filename == 'Gemfile' # assume it's a gemfile
-        parse_gemfile(content)
-      else
-        raise ArgumentError, "unknown parse strategy for #{filename}"
-      end
-    end
-
-    def parse_lockfile(content)
-      @gems = Bundler::LockfileParser.new(content).dependencies.map { |item| item[0] }
-    end
-
-    def parse_gemfile(content)
-      begin
-        t = Tempfile.new
-        t.write content
-        t.rewind
-        @gems = Bundler::Definition.build(t, nil, {}).dependencies.map(&:name)
-      ensure
-        t.close
-      end
-    end
-
-    # @todo: LOL
-    def parse_gemspec(content)
-      lines = content.scan(/add.*dependency\s*(?:'|")(.*)(?:'|")/).flatten
-      lines.each do |line|
-        @gems << line.split('"')[0].split("'")[0].split(',')[0]
-      end
+      content = Base64.decode64(JSON.parse(response.to_s)['content']).force_encoding('UTF-8')
+      @gems = LockfileParser.new(content).gem_names
     end
   end
 end
