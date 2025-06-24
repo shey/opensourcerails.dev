c78654f3893a7f828c43aaac755b399d35ccdf4e
shey <shey@shey.ca>
2025-06-23 22:28:05 -0400

    standard formatting change

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -2,8 +2,8 @@
 
 ActiveAdmin.register Project do
   permit_params(:name, :slug, :rails_major_version, :github, :github_about, :website, :contributors, :dependents, :stars,
-                :watchers, :forks, :app, :content, :description, :primary_image, :short_blurb, :color, :hidden_at, :gems_path, :packages_path,
-                stack_list: [], category_list: [], adjective_list: [])
+    :watchers, :forks, :app, :content, :description, :primary_image, :short_blurb, :color, :hidden_at, :gems_path, :packages_path,
+    stack_list: [], category_list: [], adjective_list: [])
 
   filter :name
   filter :hidden_at
@@ -17,92 +17,92 @@ ActiveAdmin.register Project do
   end
 
   Project.tag_types.each do |type|
-    scope "No #{type}", "without_tagged_#{type}".to_sym
+    scope "No #{type}", :"without_tagged_#{type}"
   end
 
   member_action :scan do
     resource.helpers.scan!
-    redirect_to [:admin, resource], notice: 'Scanned.'
+    redirect_to [:admin, resource], notice: "Scanned."
   end
 
   member_action :scrape_app do
     resource.helpers.scrape_app!
-    redirect_to [:admin, resource], notice: 'Scraped app.'
+    redirect_to [:admin, resource], notice: "Scraped app."
   end
 
   action_item :scrape_app, only: [:show] do
-    link_to 'Scrape app', scrape_app_admin_project_path(resource)
+    link_to "Scrape app", scrape_app_admin_project_path(resource)
   end
 
   member_action :scrape_last_activity do
     resource.helpers.scrape_last_activity!
-    redirect_to [:admin, resource], notice: 'Scraped last activity.'
+    redirect_to [:admin, resource], notice: "Scraped last activity."
   end
 
   action_item :scrape_last_activity, only: [:show] do
-    link_to 'Scrape last activity', scrape_last_activity_admin_project_path(resource)
+    link_to "Scrape last activity", scrape_last_activity_admin_project_path(resource)
   end
 
   action_item :scrape_meta, only: [:show] do
-    link_to 'Scrape meta', scrape_meta_admin_project_path(resource)
+    link_to "Scrape meta", scrape_meta_admin_project_path(resource)
   end
 
   member_action :scrape_meta do
     resource.helpers.scrape_meta!
-    redirect_to [:admin, resource], notice: 'Scraped meta.'
+    redirect_to [:admin, resource], notice: "Scraped meta."
   end
 
   action_item :check_pulse, only: [:show] do
-    link_to 'Check pulse', check_pulse_admin_project_path(resource)
+    link_to "Check pulse", check_pulse_admin_project_path(resource)
   end
 
   member_action :check_pulse do
     resource.helpers.check_pulse!
-    redirect_to [:admin, resource], notice: 'Checked pulse.'
+    redirect_to [:admin, resource], notice: "Checked pulse."
   end
 
   action_item :analyze_stacks, only: [:show] do
-    link_to 'Analyze stack', analyze_stacks_admin_project_path(resource)
+    link_to "Analyze stack", analyze_stacks_admin_project_path(resource)
   end
 
   member_action :analyze_stacks do
     resource.helpers.analyze_stacks!
-    redirect_to [:admin, resource], notice: 'Analyzed stack.'
+    redirect_to [:admin, resource], notice: "Analyzed stack."
   end
 
   action_item :scrape_gemfile, only: [:show] do
-    link_to 'scrape gemfile', scrape_gemfile_admin_project_path(resource)
+    link_to "scrape gemfile", scrape_gemfile_admin_project_path(resource)
   end
 
   member_action :scrape_gemfile do
     resource.helpers.scrape_gemfile!
-    redirect_to [:admin, resource], notice: 'scraped gemfile.'
+    redirect_to [:admin, resource], notice: "scraped gemfile."
   end
 
   action_item :scrape_packages, only: [:show] do
-    link_to 'scrape packages', scrape_packages_admin_project_path(resource)
+    link_to "scrape packages", scrape_packages_admin_project_path(resource)
   end
 
   member_action :scrape_packages do
     resource.helpers.scrape_packages!
-    redirect_to [:admin, resource], notice: 'scraped packages.'
+    redirect_to [:admin, resource], notice: "scraped packages."
   end
 
   member_action :colorpicker do
-    render template: 'admin/projects/colorpicker'
+    render template: "admin/projects/colorpicker"
   end
 
   action_item :colorpicker, only: [:show] do
-    link_to 'Colorpicker', colorpicker_admin_project_path(resource)
+    link_to "Colorpicker", colorpicker_admin_project_path(resource)
   end
 
   controller do
     def update
-      resource_params.first.delete(:color) if resource_params.first[:color] == '#000000'
-      if resource_params.first[:gems_path].nil? || resource_params.first[:gems_path].try(:empty?) || resource_params.first[:gems_path].try(:blank?)
-        resource.gems_path = []
+      resource_params.first.delete(:color) if resource_params.first[:color] == "#000000"
+      resource.gems_path = if resource_params.first[:gems_path].nil? || resource_params.first[:gems_path].try(:empty?) || resource_params.first[:gems_path].try(:blank?)
+        []
       else
-        resource.gems_path = resource_params.first[:gems_path]
+        resource_params.first[:gems_path]
       end
       super
     end
@@ -145,8 +145,8 @@ ActiveAdmin.register Project do
       end
       row :primary_image do
         if resource.primary_image.present?
-          div style: 'object-scale: fit-content' do
-            image_tag resource.primary_image, style: 'max-width: 400px;'
+          div style: "object-scale: fit-content" do
+            image_tag resource.primary_image, style: "max-width: 400px;"
           end
         end
       end
@@ -154,7 +154,7 @@ ActiveAdmin.register Project do
   end
 
   form do |f|
-    f.inputs 'Project' do
+    f.inputs "Project" do
       f.input :name
       f.input :slug
       f.input :description
@@ -165,29 +165,29 @@ ActiveAdmin.register Project do
       f.input :color, as: :color_picker
       f.input :hidden_at, as: :date_time_picker
     end
-    f.inputs 'Rails' do
+    f.inputs "Rails" do
       f.input :rails_major_version
     end
-    f.inputs 'Config' do
-      f.input :gems_path, as: :string, hint: 'no starting slash, something like dir/Gemfile.lock'
-      f.input :packages_path, as: :string, hint: 'no starting slash, something like dir/package.json'
+    f.inputs "Config" do
+      f.input :gems_path, as: :string, hint: "no starting slash, something like dir/Gemfile.lock"
+      f.input :packages_path, as: :string, hint: "no starting slash, something like dir/package.json"
     end
-    f.inputs 'Community' do
+    f.inputs "Community" do
       f.input :watchers
       f.input :forks
       f.input :contributors
       f.input :dependents
       f.input :stars
     end
-    f.inputs 'Taggings' do
+    f.inputs "Taggings" do
       f.input :category_list, as: :select, multiple: true, collection: TagCache.categories.pluck(:name).uniq,
-                              input_html: { 'data-tags' => true }
+        input_html: {"data-tags" => true}
       f.input :adjective_list, as: :select, multiple: true, collection: TagCache.adjectives.pluck(:name).uniq,
-                               input_html: { 'data-tags' => true }
+        input_html: {"data-tags" => true}
     end
-    f.inputs 'Content' do
+    f.inputs "Content" do
       f.li do
-        f.label :content, class: 'trix-editor-label'
+        f.label :content, class: "trix-editor-label"
         f.rich_text_area :content
       end
       f.input :primary_image, as: :file
