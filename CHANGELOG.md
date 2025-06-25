0108386441ba29ccdf27e846e37b362055d6aead
shey <shey@shey.ca>
2025-06-24 22:21:47 -0400

    CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## 2025-06-24
+
+### Added
+- Skylight URL display on project show pages
+
+## 2025-06-18
+
+### Added
+- `/feed.xml` endpoint: lists the 15 most recently added projects
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -35,8 +35,35 @@
 #  index_projects_on_hidden_at                 (hidden_at)
 #  index_projects_on_slug                      (slug) UNIQUE
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Project do
+  describe "#skylight_url" do
+    it "can store a valid Skylight URL" do
+      project = described_class.create!(
+        name: "Test Project",
+        slug: "test-project",
+        github: "test/project",
+        rails_major_version: 7,
+        data: {},
+        skylight_url: "https://oss.skylight.io/app/applications/XYZ/recent/6h/endpoints"
+      )
 
+      expect(project.skylight_url).to eq("https://oss.skylight.io/app/applications/XYZ/recent/6h/endpoints")
+    end
+
+    it "is invalid with a bad URL" do
+      project = described_class.new(
+        name: "Bad Project",
+        slug: "bad-project",
+        github: "bad/project",
+        rails_major_version: 7,
+        data: {},
+        skylight_url: "not-a-url"
+      )
+
+      expect(project).not_to be_valid
+      expect(project.errors[:skylight_url]).to be_present
+    end
+  end
 end
